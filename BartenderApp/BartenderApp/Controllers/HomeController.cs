d67eb40395f8ca0b683aa1c02fc3559d9e0c1839
Chris-Joyce <36461109+Chris-Joyce@users.noreply.github.com>
2018-08-30 23:14:16 -0400

    Add files via upload

---
+++ BartenderApp/BartenderApp/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BartenderApp.Models;
+
+namespace BartenderApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Queue<Cocktail> orders;
+        private List<Cocktail> cocktailMenu;
+        public HomeController()
+        {
+            cocktailMenu = new List<Cocktail>
+            {
+                new Cocktail()
+                { Name = "cocktail", Cost = "10.00" },
+                new Cocktail()
+                { Name = "cocktail1", Cost = "11.70" },
+                new Cocktail()
+                { Name = "cocktail2", Cost = "12.50" },
+                new Cocktail()
+                { Name = "cocktail3", Cost = "13.10" },
+                new Cocktail()
+                { Name = "cocktail4", Cost = "14.80" },
+                new Cocktail()
+                { Name = "cocktail5", Cost = "15.70" },
+                new Cocktail()
+                { Name = "cocktail6", Cost = "16.60" },
+                new Cocktail()
+                { Name = "cocktail7", Cost = "17.30" },
+                new Cocktail()
+                { Name = "cocktail8", Cost = "18.00" },
+                new Cocktail()
+                { Name = "cocktail9", Cost = "19.20" }
+            };
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return View(cocktailMenu);
+        }
+        public ActionResult OrderQueue()
+        {
+            return View(orders);
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BartenderApp/BartenderApp/Models/Cocktail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BartenderApp.Models
+{
+    public class Cocktail
+    {
+        private Queue<Cocktail> orderQueue;
+
+        public string Name { get; set; }
+        public string Cost { get; set; }
+
+        public void PlaceOrder(Cocktail order)
+        {
+            orderQueue.Enqueue(order);
+        }
+
+        public void CompleteOrder()
+        {
+            orderQueue.Dequeue();
+        }
+    }
+
+    
+}
